43ed9b41d35e8230b28b2801b8ba167662f21470
Zach Kellerman <63438523+zkellerman@users.noreply.github.com>
2024-09-10 10:22:20 -0400

    Added a button that will update the number of squares each side should have without expanding the size of the container.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,43 @@
 const container = document.querySelector(".container");
 
-for (let i = 1; i <= 256; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
+function updateGridSize(size) {
+  while (container.firstChild) {
+    container.removeChild(container.lastChild);
+    console.log('removed');
+  }
 
-  square.addEventListener("mouseover", () => {
-    square.style.backgroundColor = "black";
-  });
+  let totalSquares = size * size;
+  for (let i = 1; i <= totalSquares; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
 
-  container.appendChild(square);
+    // square size = (container width - (size * L&R 1px border)) / size
+    let squareSize = (750 - (size * 2)) / size;
+    console.log(squareSize);
+    square.style.height = `${squareSize}px`;
+    square.style.width = `${squareSize}px`;
+
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "black";
+    });
+
+    container.appendChild(square);
+  }
 }
+
+const gridInfoArea = document.querySelector(".gridInfo");
+const changeSizeButton = document.querySelector("#changeGrid");
+const gridSizeParagraph = gridInfoArea.lastElementChild;
+
+changeSizeButton.addEventListener("click", () => {
+  let promptMessage = 'Enter how many squares per side there should be (Min: 16, Max: 100):';
+  let size = prompt(promptMessage);
+  if (size < 16 || size > 100)
+    alert('Invalid size. Value must be between 16 and 100.');
+  else {
+    updateGridSize(size);
+    gridSizeParagraph.textContent = `Current Size: ${size}x${size}`;
+  }
+});
+
+updateGridSize(16);
