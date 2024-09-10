d055eed4eacf0a2470931bb57e8179de54ad609d
Zach Kellerman <63438523+zkellerman@users.noreply.github.com>
2024-09-10 09:28:42 -0400

    When the mouse passes through a square, the background color is changed to black.

--- scripts.js
+++ scripts.js
@@ -3,5 +3,10 @@ const container = document.querySelector(".container");
 for (let i = 1; i <= 256; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
+
+  square.addEventListener("mouseover", () => {
+    square.style.backgroundColor = "black";
+  });
+
   container.appendChild(square);
 }
