15483d58ae510a02feb068a193c5858ea0a329e6
Zach Kellerman <63438523+zkellerman@users.noreply.github.com>
2024-09-10 10:31:35 -0400

    The background color of each square is randomized instead of black when the user hovers over a square on the grid.

--- scripts.js
+++ scripts.js
@@ -18,11 +18,20 @@ function updateGridSize(size) {
     square.style.width = `${squareSize}px`;
 
     square.addEventListener("mouseover", () => {
-      square.style.backgroundColor = "black";
+      square.style.backgroundColor = getRandomColor();
     });
 
     container.appendChild(square);
   }
+}
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#'
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 const gridInfoArea = document.querySelector(".gridInfo");
